31d8cb16a282fa65f31dd2677ff5c5a047219fa5
Splagon <misha.higham@hotmail.co.uk>
2022-10-07 20:31:23 +0100

    Fix errors in Thing model

--- things/models.py
+++ things/models.py
@@ -1,11 +1,7 @@
 from django.db import models
 #from django.contrib.models import Model
 
-class Thing(models.Model):
-    name = models.CharField(
-        max_length = 50
-    )
-    description = models.CharField(
-        max_length = 400
-    )
-    quanitity = models.IntegerField()
+class Thing(models.Model);
+    name
+    description
+    quanitity
