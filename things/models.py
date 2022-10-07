c198db6afed9150ff53faee31b5ccbcb3d3450c4
Splagon <misha.higham@hotmail.co.uk>
2022-10-07 20:48:58 +0100

    Add constraints to variables in Thing model

--- things/models.py
+++ things/models.py
@@ -1,11 +1,22 @@
 from django.db import models
-#from django.contrib.models import Model
+from django.core.validators import MinLengthValidator
+from django.core.validators import MaxLengthValidator
 
 class Thing(models.Model):
     name = models.CharField(
-        max_length = 30
+        max_length = 30,
+        blank = False,
+        unique = True
     )
     description = models.CharField(
-        max_length = 400
+        max_length = 120,
+        blank = True,
+        unique = False
     )
-    quantity = models.IntegerField()
+    quantity = models.IntegerField(
+        unique = False,
+        validators = [
+            MinLengthValidator(0, message = "Quantity must be 0 or above"),
+            MaxLengthValidator(100, message = "Quantity must be 100 or below")
+        ]
+    )
